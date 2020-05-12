8b1ceada7b43054f83f3e97b8a6ee1278065c186
South_under_been <s2018s27@e-mirim.hs.kr>
2020-05-12 22:29:40 +0900

    05월 12일 수업 예제 07번 가장 큰 수, 작은 수 출력하기

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,40 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+
+         string input_num;
+         int max_num = 0;
+         int min_num = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.Write("숫자를 입력해주세요: ");
+                input_num = Console.ReadLine();
+                if (i == 0)
+                {
+                    max_num = int.Parse(input_num);
+                    min_num = int.Parse(input_num);
+                }
+
+                else
+                {
+                    if (int.Parse(input_num) > max_num)
+                    {
+                        max_num = int.Parse(input_num);
+                    }
+                    if (int.Parse(input_num) < min_num)
+                    {
+                        min_num = int.Parse(input_num);
+                    }
+                }
+
+            }
+
+         Console.WriteLine("-----------------------------");
+         Console.WriteLine("가장 큰 수: " + max_num);
+         Console.WriteLine("가장 작은 수: " + min_num);
+
         }
     }
 }
